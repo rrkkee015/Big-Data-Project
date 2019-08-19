613503900b0ae36a5fe348f6de287161c6b6da0d
HAN_DONG_HUN <pok_winter@naver.com>
2019-08-19 14:20:36 +0900

    탭 사이즈 조절

--- backend/api/views/movie_views.py
+++ backend/api/views/movie_views.py
@@ -8,37 +8,37 @@ from rest_framework.response import Response
 @api_view(['GET', 'POST', 'DELETE'])
 def movies(request):
 
-    if request.method == 'GET':
-        id = request.GET.get('id', request.GET.get('movie_id', None))
-        title = request.GET.get('title', None)
+  if request.method == 'GET':
+    id = request.GET.get('id', request.GET.get('movie_id', None))
+    title = request.GET.get('title', None)
 
-        movies = Movie.objects.all()
+    movies = Movie.objects.all()
 
-        if id:
-            movies = movies.filter(pk=id)
-        if title:
-            movies = movies.filter(title__icontains=title)
+    if id:
+        movies = movies.filter(pk=id)
+    if title:
+        movies = movies.filter(title__icontains=title)
 
-        serializer = MovieSerializer(movies, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    serializer = MovieSerializer(movies, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == 'DELETE':
-        movie = Movie.objects.all()
-        movie.delete()
-        return Response(status=status.HTTP_200_OK)
+  if request.method == 'DELETE':
+    movie = Movie.objects.all()
+    movie.delete()
+    return Response(status=status.HTTP_200_OK)
 
-    if request.method == 'POST':
-        movies = request.data.get('movies', None)
-        for movie in movies:
-            id = movie.get('id', None)
-            title = movie.get('title', None)
-            genres = movie.get('genres', None)
+  if request.method == 'POST':
+    movies = request.data.get('movies', None)
+    for movie in movies:
+        id = movie.get('id', None)
+        title = movie.get('title', None)
+        genres = movie.get('genres', None)
 
-            if not (id and title and genres):
-                continue
-            if Movie.objects.filter(id=id).count() > 0 or Movie.objects.filter(title=title).count() > 0:
-                continue
+        if not (id and title and genres):
+            continue
+        if Movie.objects.filter(id=id).count() > 0 or Movie.objects.filter(title=title).count() > 0:
+            continue
 
-            Movie(id=id, title=title, genres='|'.join(genres)).save()
+        Movie(id=id, title=title, genres='|'.join(genres)).save()
 
-        return Response(status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_200_OK)
