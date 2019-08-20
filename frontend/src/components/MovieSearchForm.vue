c228eacb2c9c0292d07fa912e0b6212a2108e026
HAN_DONG_HUN <pok_winter@naver.com>
2019-08-20 13:14:16 +0900

    검색 계속 돌리기

--- frontend/src/components/MovieSearchForm.vue
+++ frontend/src/components/MovieSearchForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-form ref="form">
-    <v-text-field v-model="title" label="영화 제목" />
+    <v-text-field v-model="title" label="영화 제목" @keyup.native="onSubmit" />
     <v-layout justify-center pa-10>
       <v-btn large color="indigo white--text" @click="onSubmit">Search</v-btn>
     </v-layout>
