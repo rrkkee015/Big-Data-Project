df25418384f1a93688589f56356b5aeb342c7971
HAN_DONG_HUN <pok_winter@naver.com>
2019-08-21 10:42:45 +0900

    엔터 누르면 검색하도록 만듦

--- frontend/src/components/MovieSearchForm.vue
+++ frontend/src/components/MovieSearchForm.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-form ref="form">
-    <v-text-field v-model="title" label="영화 제목" @keyup.native="onSubmit" />
+  <v-form ref="form" v-on:submit.prevent="noop">
+    <v-text-field v-model="title" label="영화 제목" @keyup.enter="onSubmit"/>
     <v-layout justify-center pa-10>
       <v-btn large color="indigo white--text" @click="onSubmit">Search</v-btn>
     </v-layout>
@@ -26,6 +26,9 @@ export default {
       };
       
       this.submit(params);
+    },
+    noop () {
+      // do nothing ?
     }
   }
 };
